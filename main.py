06ebf6665dda852d867cc57db6e87d4b6a2c0fda
Brian Duong <59269548+b7duongcs@users.noreply.github.com>
2023-08-20 17:11:52 -0400

    clean up code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Disc Golf")
 
 GREEN = (0,128,0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 FPS = 60
 TICKS_PER_FRAME = 1000/FPS
@@ -16,22 +17,34 @@ TIME_LIMIT = 10000
 
 FONT = pygame.font.Font('freesansbold.ttf', 32)
 
-DISC_SIZE = 64
+DISC_SIZE = 32
 DISC_IMAGE = pygame.image.load(os.path.join('assets', 'disc_sprite.png'))
 DISC = pygame.transform.scale(DISC_IMAGE, (DISC_SIZE, DISC_SIZE))
 
+START_X = 10
+START_Y = (HEIGHT - DISC_SIZE) / 2
 
 def draw_window(disc, colour, time):
     WIN.fill(colour)
     WIN.blit(DISC, (disc.x, disc.y))
     if time >= 0:
-        timer = 10 - int(time / 1000)
+        timer = int(TIME_LIMIT / 1000 - int(time / 1000))
         text = FONT.render(str(timer), True, WHITE)
         WIN.blit(text, (10,10))
     pygame.display.update()
 
+def controls(keys_pressed, disc):
+    if keys_pressed[pygame.K_w]:
+        disc.y -= 1
+    if keys_pressed[pygame.K_a]:
+        disc.x -= 1
+    if keys_pressed[pygame.K_s]:
+        disc.y += 1
+    if keys_pressed[pygame.K_d]:
+        disc.x += 1
+
 def main():
-    disc = pygame.Rect(4, 368, DISC_SIZE, DISC_SIZE)
+    disc = pygame.Rect(START_X, START_Y, DISC_SIZE, DISC_SIZE)
     launching = False #false: inputting controls, true: watching disc fly, no inputs
     current_time = 0
 
@@ -42,16 +55,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
         current_time = pygame.time.get_ticks()
         if current_time >= TIME_LIMIT:
             launching = True
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_d]:
-            disc.x += 1
+
         if not launching:
             draw_window(disc, GREEN, current_time)
+            keys_pressed = pygame.key.get_pressed()
+            controls(keys_pressed, disc)
         else:
-            draw_window(disc, (0,0,0), -1)
+            draw_window(disc, BLACK, -1)
 
     pygame.quit()
 
